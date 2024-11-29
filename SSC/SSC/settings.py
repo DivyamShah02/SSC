76c9e13429e7301b96e0d96f8e354d2dcd2ff557
Divyam Shah <divyamshah1234@gmail.com>
2024-11-29 21:12:33 +0530

    final chagnes regarding s3 bucket

--- SSC/SSC/settings.py
+++ SSC/SSC/settings.py
@@ -189,6 +189,7 @@ AWS_S3_SIGNATURE_NAME = 's3v4'
 AWS_S3_REGION_NAME = 'eu-north-1'
 AWS_QUERYSTRING_AUTH = False
 
+
 # S3 Media Files Configuration
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
